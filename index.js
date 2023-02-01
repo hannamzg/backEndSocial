cd05b739fc24e1e51301ee6283e30977353bf045
hanna mzeget <elimzghanna123@gmail.com>
2023-02-01 03:08:35 +0200

    fisrt

---
+++ index.js
@@ -0,0 +1,36 @@
+import  express  from  "express";
+
+const app = express();
+
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js"
+import commentRoutes from "./routes/comments.js"
+import likeRoutes from "./routes/likes.js"
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+app.use((req,res,next)=>{
+ res.header("Access-Control-Allow-Credentials",true)   
+ next()
+})
+
+app.use(express.json())
+
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
+
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
+app.use("/api/comments",commentRoutes)
+app.use("/api/likes",likeRoutes)
+
+const Port = 5000;
+
+app.listen(Port,()=>{
+    console.log("hanna "+5000);
+})
